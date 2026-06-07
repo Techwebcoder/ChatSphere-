af6e23887a8c0b41c83dfd0b3d28e1b0ea38e268
Techwebcoder <rajakumar.xia.11117@gmail.com>
2026-06-07 21:01:42 +0530

    This is the live chatting app using the websocket and stomp

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,12 @@
+package com.chat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+}
